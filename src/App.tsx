80128487c4209305f4fa6f98491e5f662f3851e1
IIIWinterIII <nikitavakulin39@gmail.com>
2025-01-04 17:26:52 +0300

    ///

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faInstagram } from '@fortawesome/free-brands-svg-icons';
 import { faScissors } from '@fortawesome/free-solid-svg-icons';
-
+ 
 
 const activeTabs: { [k: string]: boolean } = {
   "/": false,
